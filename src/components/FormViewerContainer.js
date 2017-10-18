b4181ef46197cdaf830014e57f42d97efdebec92
Scott Lis <scott.lis@gmail.com>
2017-10-17 21:19:16 -0600

    Sort points based on order, waterlines and buttocks fully integrated now

--- src/components/FormViewerContainer.js
+++ src/components/FormViewerContainer.js
@@ -5,12 +5,23 @@ import FormViewer from "./FormViewer";
 
 const mapStateToProps = (state) => {
 		const points = state.waterlines.filter((offset) => {
-			return offset && offset.x != undefined && offset.y != undefined;
+			return offset && offset.x !== undefined && offset.y !== undefined;
 		});
 
         points.push(...state.buttocks.filter((offset) => {
-            return offset && offset.x !== undefined && offset.y != undefined;
+            return offset && offset.x !== undefined && offset.y !== undefined;
         }));
+
+        points.sort((a, b) => {
+            if (a.order < b.order) {
+                return -1;
+            }
+            if (a.order > b.order) {
+                return 1;
+            }
+            // a must be equal to b
+            return 0;
+        });
 
         return {
             points: points
